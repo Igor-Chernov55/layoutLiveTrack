2f2c524920f3083ffccd5892116bb693251e74a1
clean <igorek_chernov_1998@mail.ru>
2022-12-12 13:55:13 +0300

    adaptive layout complete

---
+++ app/js/jsLanding.js
@@ -0,0 +1,178 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    speed: 200,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            // spaceBetween: 30,
+        },
+        // when window width is >= 480px
+        580: {
+            slidesPerView: 2,
+            // spaceBetween: 30,
+
+        },
+        // when window width is >= 640px
+        840: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+const swiperComment = new Swiper(".swiperComment", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    speed: 200,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        // when window width is >= 480px
+        580: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+
+        },
+        // when window width is >= 640px
+        840: {
+            slidesPerView: 3,
+            spaceBetween: 10
+        }
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
+
+const swiperTrajectologs = new Swiper(".swiperNew", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    touchAction: true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: -80,
+        },
+        // when window width is >= 480px
+        580: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+
+        },
+        // when window width is >= 640px
+        840: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
+
+const swiperPrice = new Swiper(".swiperPrice", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    touchAction: true,
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: -80,
+        },
+        // when window width is >= 480px
+        580: {
+            slidesPerView: 1,
+            spaceBetween: -80,
+        },
+        780: {
+            slidesPerView: 2,
+
+        },
+        // when window width is >= 640px
+        840: {
+            slidesPerView: 3,
+        }
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+})
+
+const swiperMap = new Swiper(".swiperMap", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    touchAction: true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: -80,
+        },
+        // when window width is >= 480px
+        580: {
+            slidesPerView: 1,
+            spaceBetween: -120,
+        },
+        780: {
+            slidesPerView: 2,
+
+        },
+        // when window width is >= 640px
+        840: {
+            slidesPerView: 2,
+        }
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+})
+
+const sliderImage = document.querySelector('.roadMap__Block-image')
+
+swiperMap.on('snapIndexChange', () => {
+
+    console.log(sliderImage)
+
+})
+
+swiperMap.on('slideNextTransitionStart', () => {
+
+        sliderImage.className+=' imageWay-changeNext1';
+
+})
+
+swiperMap.on('slidePrevTransitionStart', () => {
+
+        sliderImage.classList.remove('imageWay-changeNext1')
+
+})
